2453b9589dc52da03fd021795f3cd8aeecb245c4
mateuszr87 <mateuszr87@gmail.com>
2018-10-01 20:31:09 +0200

    initial commit

---
+++ components/login_form.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <input 
+      id="login" 
+      name="login" >
+    <input 
+      id="password" 
+      name="password" >
+    <b-alert show>Default Alert</b-alert>
+  </div>
+</template>
